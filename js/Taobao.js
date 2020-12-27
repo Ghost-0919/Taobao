981717f367954443fc97fb3bfe3e3a898a65d31b
Ghost-0919 <2475234537@qq.com>
2020-12-27 16:41:26 +0800

    淘宝首页增加鼠标悬浮事件

--- js/Taobao.js
+++ js/Taobao.js
@@ -37,3 +37,161 @@ function serv() {
 function serv1() {
     document.getElementById("service").style.display = "none"
 }
+
+function navig() {
+    document.getElementById("navigation").style.display = "block"
+}
+
+function navig1() {
+    document.getElementById("navigation").style.display = "none"
+}
+
+function serv1_1() {
+    document.getElementById("serv_cont").style.display = "block"
+    document.getElementById("service1").style.display = "block"
+}
+
+function serv1_2() {
+    document.getElementById("serv_cont").style.display = "none"
+    document.getElementById("service1").style.display = "none"
+}
+
+function serv2_1() {
+    document.getElementById("serv_cont1").style.display = "block"
+    document.getElementById("service2").style.display = "block"
+}
+
+function serv2_2() {
+    document.getElementById("serv_cont1").style.display = "none"
+    document.getElementById("service3").style.display = "none"
+}
+
+function serv3_1() {
+    document.getElementById("serv_cont2").style.display = "block"
+    document.getElementById("service3").style.display = "block"
+}
+
+function serv3_2() {
+    document.getElementById("serv_cont2").style.display = "none"
+    document.getElementById("service3").style.display = "none"
+}
+
+function serv4_1() {
+    document.getElementById("serv_cont3").style.display = "block"
+    document.getElementById("service4").style.display = "block"
+}
+
+function serv4_2() {
+    document.getElementById("serv_cont3").style.display = "none"
+    document.getElementById("service4").style.display = "none"
+}
+
+function serv5_1() {
+    document.getElementById("serv_cont4").style.display = "block"
+    document.getElementById("service5").style.display = "block"
+}
+
+function serv5_2() {
+    document.getElementById("serv_cont4").style.display = "none"
+    document.getElementById("service5").style.display = "none"
+}
+
+function serv6_1() {
+    document.getElementById("serv_cont5").style.display = "block"
+    document.getElementById("service6").style.display = "block"
+}
+
+function serv6_2() {
+    document.getElementById("serv_cont5").style.display = "none"
+    document.getElementById("service6").style.display = "none"
+}
+
+function serv7_1() {
+    document.getElementById("serv_cont6").style.display = "block"
+    document.getElementById("service7").style.display = "block"
+}
+
+function serv7_2() {
+    document.getElementById("serv_cont6").style.display = "none"
+    document.getElementById("service7").style.display = "none"
+}
+
+function serv8_1() {
+    document.getElementById("serv_cont7").style.display = "block"
+    document.getElementById("service8").style.display = "block"
+}
+
+function serv8_2() {
+    document.getElementById("serv_cont7").style.display = "none"
+    document.getElementById("service8").style.display = "none"
+}
+
+function serv9_1() {
+    document.getElementById("serv_cont8").style.display = "block"
+    document.getElementById("service9").style.display = "block"
+}
+
+function serv9_2() {
+    document.getElementById("serv_cont8").style.display = "none"
+    document.getElementById("service9").style.display = "none"
+}
+
+function serv10_1() {
+    document.getElementById("serv_cont9").style.display = "block"
+    document.getElementById("service10").style.display = "block"
+}
+
+function serv10_2() {
+    document.getElementById("serv_cont9").style.display = "none"
+    document.getElementById("service10").style.display = "none"
+}
+
+function serv11_1() {
+    document.getElementById("serv_cont10").style.display = "block"
+    document.getElementById("service11").style.display = "block"
+}
+
+function serv11_2() {
+    document.getElementById("serv_cont10").style.display = "none"
+    document.getElementById("service11").style.display = "none"
+}
+
+function serv12_1() {
+    document.getElementById("serv_cont11").style.display = "block"
+    document.getElementById("service12").style.display = "block"
+}
+
+function serv12_2() {
+    document.getElementById("serv_cont11").style.display = "none"
+    document.getElementById("service12").style.display = "none"
+}
+
+function serv13_1() {
+    document.getElementById("serv_cont12").style.display = "block"
+    document.getElementById("service13").style.display = "block"
+}
+
+function serv13_2() {
+    document.getElementById("serv_cont12").style.display = "none"
+    document.getElementById("service13").style.display = "none"
+}
+
+function serv14_1() {
+    document.getElementById("serv_cont13").style.display = "block"
+    document.getElementById("service14").style.display = "block"
+}
+
+function serv14_2() {
+    document.getElementById("serv_cont13").style.display = "none"
+    document.getElementById("service14").style.display = "none"
+}
+
+function serv15_1() {
+    document.getElementById("serv_cont14").style.display = "block"
+    document.getElementById("service15").style.display = "block"
+}
+
+function serv15_2() {
+    document.getElementById("serv_cont14").style.display = "none"
+    document.getElementById("service15").style.display = "none"
+}
